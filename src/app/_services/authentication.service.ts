62170298f30dca13b1210f4c8424c53d985cbbb5
hvqkhai <huynhkhai6678@gmail.com>
2018-11-19 22:11:36 +0700

    Skeleton for Cashier POS

---
+++ src/app/_services/authentication.service.ts
@@ -0,0 +1,54 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { environment } from '@environments/environment';
+import { User } from '@app/_models';
+
+@Injectable({ providedIn: 'root' })
+export class AuthenticationService {
+    private currentUserSubject: BehaviorSubject<User>;
+    public currentUser: Observable<User>;
+    private httpOptions;
+
+    constructor(private http: HttpClient) {
+        this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+        this.currentUser = this.currentUserSubject.asObservable();
+        this.httpOptions = {
+            headers: new HttpHeaders({
+                'Content-Type': 'application/json'
+            })
+        };
+    }
+
+    public get currentUserValue(): User {
+        return this.currentUserSubject.value;
+    }
+
+    login(email: string, password: string) {
+        console.log(email + password);
+        let param = { email: email, password: password };
+        return this.http.post(`${environment.apiUrl}/user/cashier/login`, { email, password })
+            .pipe(map(response => {
+                //login successful if there's a jwt token in the response
+                if (response && response['tokenId']) {
+                    // store user details and jwt token in local storage to keep user logged in between page refreshes
+                    localStorage.setItem('currentUser', JSON.stringify(response['tokenId']));
+                    this.currentUserSubject.next(response['tokenId']);
+                }
+
+                return response;
+            }));
+        // return this.http.post(`${environment.apiUrl}/user/cashier/login`, param, { headers: hdrs, observe: 'response' }).subscribe(res => {
+        //     console.log(res);
+        //     console.log(res.headers.get('X-Powered-By'));
+        //   });        
+    }
+
+    logout() {
+        // remove user from local storage to log user out
+        localStorage.removeItem('currentUser');
+        this.currentUserSubject.next(null);
+    }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+﻿import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule }    from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+
+import { AppComponent }  from './app.component';
+import { routing }        from './app.routing';
+
+import { AlertComponent } from './_components';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';;
+import { ServiceWorkerModule } from '@angular/service-worker'
+;
+import { environment } from '../environments/environment'
+@NgModule({
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        routing,
+        ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })    ],
+    declarations: [
+        AppComponent,
+        AlertComponent,
+        HomeComponent,
+        LoginComponent,
+    ],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    ],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule { }
